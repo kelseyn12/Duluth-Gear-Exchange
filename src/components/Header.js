2e05284317a7d5329019f097620fd76a26824720
kelsey nocek <kelseyn12@hotmail.com>
2022-07-09 16:22:13 -0500

    added Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <h1>Buy Gear. Sell Gear.</h1>
+    <h2>Contentful banner image</h2>
+    </>
+  )
+}
+
+export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Header from "../components/Header"
 import Layout from "../components/Layout"
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
   <Layout>
     
    <h1>Main Page </h1>
+   <Header />
   </Layout>
   )
 }
