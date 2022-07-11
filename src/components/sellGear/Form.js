3cc5d8142f37923b31006f29b28ee85f5e1eeef2
kelsey nocek <kelseyn12@hotmail.com>
2022-07-11 12:46:05 -0500

    added form component

---
+++ src/components/sellGear/Form.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <>
+    <h2>Save time! Fill out the consignment form at home, then either print it to bring in or email it to glgecounter@gmail.com.     
+    </h2>
+    <h3>Put form here</h3>
+    </>
+  )
+}
+
+export default Form
--- src/pages/sellYourGear.js
+++ src/pages/sellYourGear.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import Form from '../components/sellGear/Form'
 import FrequentQuestions from '../components/sellGear/FrequentQuestions'
 import Section from '../components/sellGear/Section'
 
@@ -9,6 +10,7 @@ const sellYourGear = () => {
       
       <Section />
       <FrequentQuestions />
+      <Form />
 
     </Layout>
   )
