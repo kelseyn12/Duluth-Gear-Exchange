190afc14018e099973d60c8dfa4a8877df88aad8
kelsey nocek <kelseyn12@hotmail.com>
2022-07-11 12:39:06 -0500

    added frequent questions component

---
+++ src/components/sellGear/FrequentQuestions.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FrequentQuestions = () => {
+  return (
+    <>
+    <h1>FREQUENTLY ASKED QUESTIONS</h1>
+   <h4>Contentful question and response content model</h4>
+
+    
+    </>
+  )
+}
+
+export default FrequentQuestions
--- src/pages/sellYourGear.js
+++ src/pages/sellYourGear.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import FrequentQuestions from '../components/sellGear/FrequentQuestions'
 import Section from '../components/sellGear/Section'
 
 const sellYourGear = () => {
@@ -7,6 +8,7 @@ const sellYourGear = () => {
     <Layout>
       
       <Section />
+      <FrequentQuestions />
 
     </Layout>
   )
