584e19d509eff95e622fd19903ae2bf9144f88ca
kelsey nocek <kelseyn12@hotmail.com>
2022-07-16 09:16:48 -0500

    gatsby config contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,43 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  accessToken:
+    process.env.CONTENTFUL_ACCESS_TOKEN ||
+    process.env.CONTENTFUL_DELIVERY_TOKEN,
+};
+
+if (process.env.CONTENTFUL_HOST) {
+  contentfulConfig.host = process.env.CONTENTFUL_HOST;
+  contentfulConfig.accessToken = proccess.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN;
+}
+
+const { spaceId, accessToken } = contentfulConfig;
+
+if (!spaceId || !accessToken) {
+  throw new Error(
+    "Contentful spaceId and the access token need to be provided."
+  );
+}
 
 
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
-}
+  siteMetadata: {
+    title: "Duluth Gear Exchange",
+    description: "Duluth Gear Exchange Website"
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-source-contentful",
+      options: contentfulConfig,
+    },
+  ],
+};
