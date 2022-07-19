68838ddf9ce997bf529eafb25b2688fc91cdfe95
kelsey nocek <kelseyn12@hotmail.com>
2022-07-19 15:26:47 -0500

    added form

--- src/components/contact/Form.js
+++ src/components/contact/Form.js
@@ -3,7 +3,17 @@ import React from 'react'
 const Form = () => {
   return (
    <>
-   <h4>Form included here</h4>
+   <form 
+   name="contact" 
+   method="post" 
+   data-netlify="true"
+   data-netlify-honeypot="bot-field"
+   >
+    <input name="name" placeholder="Your Name" type="text" />
+    <input name="email" placeholder='name@name.com' type="email" />
+    <textarea name= "message" placeholder="I love Duluth Gear Exchange!"/>
+    <button>Send</button>
+   </form>
    </>
   )
 }
