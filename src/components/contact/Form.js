0dc49771db5e7a266afe8dcace8cc824ff5e4bc2
kelsey nocek <kelseyn12@hotmail.com>
2022-07-19 16:02:12 -0500

    added bootstrap to form and added form groups

--- src/components/contact/Form.js
+++ src/components/contact/Form.js
@@ -1,19 +1,35 @@
 import React from 'react'
+import { Form } from "react-bootstrap"
 
 const Form = () => {
   return (
    <>
-   <form 
+   <Form 
    name="contact" 
    method="post" 
    data-netlify="true"
    data-netlify-honeypot="bot-field"
    >
-    <input name="name" placeholder="Your Name" type="text" />
-    <input name="email" placeholder='name@name.com' type="email" />
-    <textarea name= "message" placeholder="I love Duluth Gear Exchange!"/>
-    <button>Send</button>
-   </form>
+    <Form.Group controlId="formBasicFirstName">
+      <Form.Label>First Name</Form.Label>
+      <Form.Control type="text" placeholder="Jane" />
+    </Form.Group>
+    <Form.Group controlId="formBasicLastName">
+      <Form.Label>Last Name</Form.Label>
+      <Form.Control type="text" placeholder="Doe" />
+    </Form.Group>
+    <Form.Group controlId="formBasicEmail">
+      <Form.Label>Email Address</Form.Label>
+      <Form.Control type="email" placeholder="name@name.com" />
+    </Form.Group>
+    <Form.Group controlId="formBasicTextBox">
+      <Form.Label>Message</Form.Label>
+      <Form.Control 
+      as="textarea"
+      rows="3"
+      placeholder="I love Duluth Gear Exchange!" />
+    </Form.Group>
+   </Form>
    </>
   )
 }
