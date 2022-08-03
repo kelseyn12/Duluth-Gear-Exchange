8aa8348b37cb267a551dd48831f809e16fe1fd83
kelsey nocek <kelseyn12@hotmail.com>
2022-08-03 12:46:04 -0500

    child markdown remark

--- src/components/sellGear/FrequentQuestions.js
+++ src/components/sellGear/FrequentQuestions.js
@@ -1,46 +1,49 @@
-import React from 'react'
+import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import Accordion from 'react-bootstrap/Accordion';
+import Accordion from "react-bootstrap/Accordion"
 
 const FrequentQuestions = () => {
-
   const data = useStaticQuery(graphql`
-  query {
-    allContentfulQuestionAnswer {
-      edges {
-        node {
-          question
-          id
-          answer {
-            answer
-            childrenMarkdownRemark {
-              html
-              id
+    query {
+      allContentfulQuestionAnswer {
+        edges {
+          node {
+            question
+            id
+            answer {
+              answer
+              childrenMarkdownRemark {
+                html
+                id
+              }
             }
           }
         }
       }
     }
-  }
-`)
+  `)
 
   return (
     <>
-    <h1>FREQUENTLY ASKED QUESTIONS</h1>
-    {data.allContentfulQuestionAnswer.edges.map( ({ node, index }) => (
+      <h1>FREQUENTLY ASKED QUESTIONS</h1>
+      {data.allContentfulQuestionAnswer.edges.map(({ node, index }) => (
         <div className="repairCost">
           <Accordion>
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>{ node.question }</Accordion.Header>
-        <Accordion.Body>
-        { node.answer.answer }
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>
+                <p>{node.question}</p>
+              </Accordion.Header>
+              <Accordion.Body>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: node.answer.childrenMarkdownRemark.html
+                  }}
+                />
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
         </div>
       ))}
-
-    
     </>
   )
 }
