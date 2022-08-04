a36c43a9ff82f53227bcdef521782c5be4eff693
kelsey nocek <kelseyn12@hotmail.com>
2022-08-04 12:44:41 -0500

    added images to homepage header

--- src/components/homePage/Header.js
+++ src/components/homePage/Header.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import { useStaticQuery, graphql } from "gatsby"
+import { GatsbyImage } from 'gatsby-plugin-image'
 
 const Header = () => {
+  const data = useStaticQuery(graphql`
+  query
+  {
+    contentfulCarousel(title: {eq: "homepage"}) {
+      Images {
+        id
+        gatsbyImageData
+        description
+      }
+    }
+  }
+  `)
   return (
     <>
     <h1>Buy Gear. Sell Gear.</h1>
-    <h2>Contentful banner image</h2>
+    <h2>Insert Automatic Carousel for banner images</h2>
+
+    {data.contentfulCarousel.Images.map( (node, index) => {
+      console.log(node)
+      return (
+      <div key={index}>
+      <GatsbyImage
+     image={ node.gatsbyImageData } 
+     alt={ node.description}
+      />
+       
+      </div>
+      )
+      })}
     </>
   )
 }
