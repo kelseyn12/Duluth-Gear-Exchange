10f84b6b01fc6265a1164f9476b133bfd6f26aec
kelsey nocek <kelseyn12@hotmail.com>
2022-08-09 14:19:27 -0500

    added looking for/not accepting content model

--- src/components/sellGear/Section.js
+++ src/components/sellGear/Section.js
@@ -1,13 +1,49 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { GatsbyImage } from 'gatsby-plugin-image'
 
 const Section = () => {
-  
+  const data = useStaticQuery(graphql`
+  query
+  {
+    allContentfulLookingForNotAccepting {
+      edges {
+        node {
+          lookingFor {
+            gatsbyImageData
+            description
+          }
+          notAccepting {
+            gatsbyImageData
+            description
+          }
+        }
+      }
+    }
+  }
+  `)
 
   return (
     <>
       <h1>Dust off your old gear. Give it new life.</h1>
       <h3>Looking For/Not Accepting Content Model</h3>
-
+      {data.allContentfulLookingForNotAccepting.edges.map( ( {node, index} ) => {
+        return (
+          <div key={index} className="lookingForNotAccepting">
+         
+        <GatsbyImage
+     image={ node.lookingFor.gatsbyImageData } 
+     alt={ node.description}
+      />
+      <GatsbyImage
+     image={ node.notAccepting.gatsbyImageData } 
+     alt={ node.description}
+      />
+        </div>
+        )})
+        }
+        
+      
       <h2>
         Never consigned?
         <br />
