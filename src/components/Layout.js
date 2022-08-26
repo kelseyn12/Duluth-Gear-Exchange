a2cceb9d385d36c53863c917966884a4e4ae784a
kelsey nocek <kelseyn12@hotmail.com>
2022-08-26 16:31:09 -0500

    added font

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navigation from './Navbar'
 import Footer from './Footer'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styles/global.css'
 
 const Layout = ({children}) => {
   return (
