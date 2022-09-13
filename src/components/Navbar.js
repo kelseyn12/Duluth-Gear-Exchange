4b138ddf1d3a99bce11998af712064c7a4a389b8
kelseyn12 <kelseynocek12@gmail.com>
2022-09-13 16:34:52 -0500

    changed nav logo location

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navigation = () => {
       <Navbar collapseOnSelect expand="lg">
         <Navbar.Brand href="/">
           <img
-            src={"../logo.png"}
+            src={"../../logocopy.png"}
             className="logo"
             alt="Duluth Gear Exchange logo"
           />
