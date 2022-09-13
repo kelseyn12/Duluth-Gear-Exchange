eaf8089e139eacf18c3b1e45a39b1043a13a8b51
kelseyn12 <kelseynocek12@gmail.com>
2022-09-13 15:51:22 -0500

    changed name of logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navigation = () => {
       <Navbar collapseOnSelect expand="lg">
         <Navbar.Brand href="/">
           <img
-            src={"../Logo.png"}
+            src={"../logo.png"}
             className="logo"
             alt="Duluth Gear Exchange logo"
           />
