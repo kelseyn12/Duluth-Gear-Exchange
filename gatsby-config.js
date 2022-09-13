e708f3eea6018d217e8b027e7faae4d1db26cec6
kelseyn12 <kelseynocek12@gmail.com>
2022-09-13 13:06:13 -0500

    added manifest and a new logo for the browser tab

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,6 +59,18 @@ module.exports = {
           endpoint: process.env.MAILCHIMP_ENDPOINT 
       },
   },
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `gatsby-starter-default`,
+      short_name: `starter`,
+      start_url: `/`,
+      background_color: `#663399`,
+      theme_color: `#663399`,
+      display: `minimal-ui`,
+      icon: `src/images/Logo.png`
+    },
+  }
     
   ],
 };
