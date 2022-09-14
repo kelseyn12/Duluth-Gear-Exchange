8cdbe37bb74a5372c9819d7a93c4ac851d53039e
kelseyn12 <kelseynocek12@gmail.com>
2022-09-14 15:31:32 -0500

    changed order of imports

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,8 @@
 import React from "react"
 import Navigation from "./Navbar"
 import Footer from "./Footer"
-import "bootstrap/dist/css/bootstrap.min.css"
 import "../styles/global.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const Layout = ({ children }) => {
   return (
